a047d4d008c07bce2ea31a330dd5caab198df8c3
DELL E5480 <tamneki8841@gmail.com>
2025-03-28 23:03:19 +0200

    Complete

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -10,5 +10,32 @@
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
-
-# TODO здесь ваш код
+th_0 = '30'
+th_1 = '31'
+lol = '28(29)'
+if month == 1:
+    print('He has', th_1, 'days')
+elif month == 2:
+    print('He has', lol, 'days')
+elif month == 3:
+    print('He has', th_1, 'days')
+elif month == 4:
+    print('He has', th_0, 'days')
+elif month == 5:
+    print('He has', th_1, 'days')
+elif month == 6:
+    print('He has', th_0, 'days')
+elif month == 7:
+    print('He has', th_1, 'days')
+elif month == 8:
+    print('He has', th_1, 'days')
+elif month == 9:
+    print('He has', th_0, 'days')
+elif month == 10:
+    print('He has', th_1, 'days')
+elif month == 11:
+    print('He has', th_0, 'days')
+elif month == 12:
+    print('He has', th_1, 'days')
+else:
+    print("Month's number uncorrect")

f0b05eadeadb8b06f6ff9cc01bf0e3d92d233f66
DELL E5480 <tamneki8841@gmail.com>
2025-05-02 23:07:16 +0300

    :) good game

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,5 +10,20 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
+a = 0
+n = 9
+All_expenses = expenses
+print(All_expenses)
+while a < n:
+    incruese_expense = (expenses * 0.03)
+    expenses = expenses + incruese_expense
+    All_expenses = All_expenses + expenses
+    educational_grant = educational_grant + 10000
+    print("Educational_grant: ",educational_grant)
+    print("Incruese expense: ",round(incruese_expense))
+    print("Expenses: ",round(expenses))
+    print("Global expenses: ",round(All_expenses))
+    a += 1
 
-# TODO здесь ваш код
+All_needs_money = All_expenses - educational_grant
+print("Student must asked",round(All_needs_money,2),"money")
